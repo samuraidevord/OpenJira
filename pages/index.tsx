8d59933c25383a39d448b41887afda855f402c4b
samuraidevord <samurai.dev.ord@gmail.com>
2022-12-25 17:09:57 +0100

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 // NextJS
-import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+// Material UI
+import { Grid, Typography, Card, CardHeader } from "@mui/material";
 import { Layout } from "../components/layouts";
 const HomePage: NextPage = () => {
   return (
-    <Layout title="Open Jira | Samurai.dev.ord">
+    <Layout title="Home | Open Jira">
       <Typography variant="h1" color="primary">
-        Hola Mundo
+        <Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardHeader title="Pendientes"></CardHeader>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardHeader title="En Progreso"></CardHeader>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardHeader title="Completadas"></CardHeader>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
       </Typography>
     </Layout>
   );
