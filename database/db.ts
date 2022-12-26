fb2da8b74a025565d3106fff5c8f82aa35317925
samuraidevord <samurai.dev.ord@gmail.com>
2022-12-26 21:31:47 +0100

    Samurai commit

--- database/db.ts
+++ database/db.ts
@@ -8,9 +8,7 @@ import mongoose from "mongoose";
  */
 
 export const connect = async () => {
-  await mongoose.connect(
-    "mongodb+srv://admin:RrnpBdXszdmKrvnz@open-jira-bbdd.uoxf74n.mongodb.net/entries-bbdd?retryWrites=true&w=majority"
-  );
+  await mongoose.connect(process.env.MONGO_URL!);
 };
 
 export const disconnect = async () => {
